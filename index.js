97f245f3d43521919381ef18c564488bdd76ee53
Crypt0Bennie <167595945+Jabr0nie@users.noreply.github.com>
2025-03-14 13:25:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ checkFarmStatus();
 // Set an interval to call checkFarmStatus every X milliseconds (e.g., 30 seconds)
 setInterval(() => {
     checkFarmStatus();
-}, 10000);
+}, 3000);
 
 const express = require('express');
 const app = express();
