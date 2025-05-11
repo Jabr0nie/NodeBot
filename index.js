646b1f346044c9c15f4b4562a20c74b9ecec02ba
Crypt0Bennie <sammyllc19@gmail.com>
2025-05-11 00:09:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,26 @@ const { Web3 } = require('web3');
 const config = require('./config');
 const BOTControllerABI = require('./Abi');
 
-const web3 = new Web3(`https://mantle-mainnet.infura.io/v3/${config.infuraProjectId}`);
+const web3 = new Web3(`https://optimism-mainnet.infura.io/v3/${config.infuraProjectId}`);
 
 const _from = "0xDf4023c082F6E1562fb228D7b961D97aF8708d4c";
 const privateKey = `${config.privateKey}`;
 
 // Define the bot addresses (Controller - not actual SC)
-const WETHMETH = "0xABc52832315E6cFbD2a8fC2A491dd830858A1190";
-const WMNTMETH = "0xa92daeD32FFB7D93b85f8B689ab540CcC2148D95";
-//0xa92daeD32FFB7D93b85f8B689ab540CcC2148D95 - autocompound - custom percent
-//0x0ceCe3b3008C877D5351713fBa395674f5F5C590 - Sends to admin
-const WMNTUSDT = "0x42738fe767EBAE1FF3F10E1bFB480433385d1C78";
-//0x42738fe767EBAE1FF3F10E1bFB480433385d1C78 - autocompound 50% send 50% to admin
-const JOEWMNT = "0x97A0701A44e5311f5A5D781e04036987A9D93e72";
-//0x97A0701A44e5311f5A5D781e04036987A9D93e72 - autocompound - custom percent
-const AUSDWMNT = "0x8062F0c0Ebe5C28321C03440A83ef4107c2686B4";
-//0x8062F0c0Ebe5C28321C03440A83ef4107c2686B4 - autocompound - custom percent - w CIRCUIT BREAKER
-//0xCcCfCfCDF1d5CBd1efba3729e5cCcCf150281Dc1 - autocompound - custom percent
-const INTXUSDT = "0x7167F93c317F693d0AaE98D0F80ABA215e5267a7";
-//0x7167F93c317F693d0AaE98D0F80ABA215e5267a7 - autocompound - custom percent
-const COOKWMNT = "0x45581E4097a9Be0BAc2CDFB1de286f97b00e6211";
-//0x45581E4097a9Be0BAc2CDFB1de286f97b00e6211 - autocompound - custom percent
+const USDCVELO = "0x9b44dfc310ba41b2f4729343353cdc3695e1ffa8";
+
 
 // Create an array of bot addresses
-const BOTAddress = [WMNTMETH];
+const BOTAddress = [USDCVELO];
 
 let isRunning = false;  // Flag to track if the function is currently running
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 // Function to check farm status and rebalance if necessary
 async function checkFarmStatus() {
-    if (isRunning) return;  // If the function is still running, skip execution
+    if (isRunning) return; // If the function is still running, skip execution
 
-    isRunning = true;  // Set the flag to indicate the function is running
+    isRunning = true; // Set the flag to indicate the function is running
 
     for (let i = 0; i < BOTAddress.length; i++) {
         console.log("Checking Farm " + i);
@@ -45,17 +32,32 @@ async function checkFarmStatus() {
         try {
             // Call the checkFarm method to get the farm status
             let inRange = await BOT.methods.checkFarm().call();
+            //let inRange = false; // Replace with actual call when ready
             console.log(`Farm ${i} in range status: `, inRange);
 
-            // If the farm is not in range, we proceed with the rebalance transaction
-            if (inRange == false) {
+            if (inRange === false) {
+
+                const txData = BOT.methods.UpdatePosition().encodeABI();
+
+
+                const gas = await BOT.methods.UpdatePosition().estimateGas({
+                    from: _from,
+                });
+
+
+                const gasWithBuffer = Math.floor(Number(gas) * 1.1); // Convert BigInt to number
+
+
+                const maxPriorityFeePerGas = 200000000; 
+                const maxFeePerGas = 200000000; 
+
                 const tx = {
                     from: _from,
                     to: BOTAddress[i],
-                    gas: '11848960664',  // Use the estimated gas here
-                    data: BOT.methods.rebalance().encodeABI(),
-                    maxPriorityFeePerGas: '20000000',
-                    maxFeePerGas: '20000000'
+                    gas: gasWithBuffer, 
+                    data: txData,
+                    maxPriorityFeePerGas: maxPriorityFeePerGas, 
+                    maxFeePerGas: maxFeePerGas 
                 };
 
                 // Sign the transaction
@@ -65,20 +67,16 @@ async function checkFarmStatus() {
                 // Send the signed transaction
                 const receipt = await web3.eth.sendSignedTransaction(signature.rawTransaction);
                 console.log('Rebalance transaction successful. Receipt:', receipt);
-
-          
-              
             }
-        }catch (error) {
+        } catch (error) {
             console.error('Error occurred during transaction:', error.message);
             if (error.message.includes('execution reverted')) {
                 console.error('Transaction reverted. It could be due to contract logic or state conditions.');
             }
-
         }
     }
     isRunning = false;
-};
+}
 
 checkFarmStatus();
 
@@ -86,7 +84,7 @@ checkFarmStatus();
 // Set an interval to call checkFarmStatus every X milliseconds (e.g., 30 seconds)
 setInterval(() => {
     checkFarmStatus();
-}, 1000);
+}, 5000);
 
 const express = require('express');
 const app = express();
